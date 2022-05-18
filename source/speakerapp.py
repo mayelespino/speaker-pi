e41ac48526762d51feb1c6484b6c008d87242712
Mayel Espino <mayel.espino@gmail.com>
2022-05-17 21:06:30 -0700

    added stations, and station categories.

--- source/speakerapp.py
+++ source/speakerapp.py
@@ -18,68 +18,6 @@ def root():
 # Internet Radio
 #
 
-@app.route('/scanner/', methods=['POST'])
-def play_scanner():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://198.178.123.20:10488/listen.pls"])
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://104.167.4.67:8296/listen.pls"])
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://162.244.80.118:9196/listen.pls"])
-    return "play_scanner - OK"
-
-@app.route('/nature/', methods=['POST'])
-def play_nature():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://94.23.252.14:8067/listen.pls"])
-    return "play_nature - OK"
-
-@app.route('/pink/', methods=['POST'])
-def play_pink():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://uk1.internet-radio.com:8004/listen.pls"])
-    return "play_pink - OK"
-
-@app.route('/bible/', methods=['POST'])
-def play_bible():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://108.163.223.242:8309/listen.pls"])
-    return "play_bible - OK"
-
-@app.route('/biblia/', methods=['POST'])
-def play_bibla():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://94.23.6.53:8358/listen.pls"])
-    return "play_biblia - OK"
-
-@app.route('/birds/', methods=['POST'])
-def play_birds():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://209.126.119.76:8200/listen.pls"])
-    return "play_birds - OK"
-
-@app.route('/ocean/', methods=['POST'])
-def play_ocean():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://5.39.71.159:8157/listen.pls"])
-    return "play_ocean - OK"
-
-@app.route('/aperadio/', methods=['POST'])
-def play_aperadio():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://dallas.myautodj.com:8099/listen.pls"])
-    return "play_aperadio - OK"
-
-@app.route('/kcbs/', methods=['POST'])
-def play_kcbs():
-    subprocess.Popen(["/usr/bin/omxplayer","http://19273.live.streamtheworld.com/KCBSAMAAC_SC"])
-    return "play_kcbs - OK"
-
-@app.route('/kqed/', methods=['POST'])
-def play_kqed():
-    subprocess.Popen(["/usr/bin/omxplayer","https://streams.kqed.org/kqedradio"])
-    return "play_kqed - OK"
-
-@app.route('/dogs/', methods=['POST'])
-def play_dogrelax():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://yp.shoutcast.com/sbin/tunein-station.pls?id=1794904"])
-    return "play_dogrelax - OK"
-
-@app.route('/talking/', methods=['POST'])
-def play_talking():
-    subprocess.Popen(["/usr/bin/mplayer","-playlist","http://74.50.122.103:7372/listen.pls"])
-    return "play_talking - OK"
-
 #
 # Volume
 #
@@ -116,52 +54,6 @@ def vol_50():
     return "vol_50 -OK"
 
 #
-# SIRI
-#
-
-@app.route('/siri_news/', methods=['POST'])
-def siri_news():
-    subprocess.Popen(["/usr/bin/mplayer","/media/mp3/text2speach/siri_news.mp3"])
-    return "siri_news -  OK"
-
-@app.route('/siri_stop/', methods=['POST'])
-def siri_stop():
-    subprocess.Popen(["/usr/bin/mplayer","/media/mp3/text2speach/siri_stop.mp3"])
-    return "siri_stop -  OK"
-
-@app.route('/siri_waitwait/', methods=['POST'])
-def siri_waitwait():
-    subprocess.Popen(["/usr/bin/mplayer","/media/mp3/text2speach/siri_play_waitwait.mp3"])
-    return "siri_waitwait -  OK"
-
-#
-# Google assistant
-#
-
-@app.route('/google_news/', methods=['POST'])
-def google_news():
-    subprocess.Popen(["/usr/bin/mplayer","/media/mp3/text2speach/google_news.mp3"])
-    return "google_news -  OK"
-
-@app.route('/google_stop/', methods=['POST'])
-def google_stop():
-    subprocess.Popen(["/usr/bin/mplayer","/media/mp3/text2speach/google_stop.mp3"])
-    return "google_stop -  OK"
-
-#
-# Text to speach
-#
-@app.route('/whoisit/', methods=['POST'])
-def say_whoisit():
-    subprocess.Popen(["/usr/bin/mplayer","/media/mp3/text2speach/who_is_it.mp3"])
-    return "say_whoisit -  OK"
-
-@app.route('/quienes/', methods=['POST'])
-def say_quienes():
-    subprocess.Popen(["/usr/bin/mplayer","/media/mp3/text2speach/quien_es.mp3"])
-    return "say_quienes -  OK"
-
-#
 # Utils
 #
 @app.route('/cron/', methods=['POST'])
@@ -177,7 +69,7 @@ def util_date_time():
     return out
 
 #
-# station
+# Play a station
 #
 @app.route('/play_station/<station>', methods=['POST'])
 def play_station(station):
@@ -187,10 +79,47 @@ def play_station(station):
     stat = dict[station]
     subprocess.Popen(["/usr/bin/mplayer","-playlist", stat])
     return "play_station: {} - OK".format(stat)
-
+#
+# List stations
+#
 @app.route('/list_stations/', methods=['GET'])
 def list_station():
     stationsDict = app.config["STATIONS"]
+    stationList = list(stationsDict.keys()) 
+    stationList.sort()
+    return ",".join(stationList)
+
+@app.route('/list_nature_stations/', methods=['GET'])
+def list_station():
+    stationsDict = app.config["NATURE"]
+    stationList = list(stationsDict.keys()) 
+    stationList.sort()
+    return ",".join(stationList)
+
+@app.route('/list_chill_stations/', methods=['GET'])
+def list_station():
+    stationsDict = app.config["CHILL"]
+    stationList = list(stationsDict.keys()) 
+    stationList.sort()
+    return ",".join(stationList)
+
+@app.route('/list_talk_stations/', methods=['GET'])
+def list_station():
+    stationsDict = app.config["TALK"]
+    stationList = list(stationsDict.keys()) 
+    stationList.sort()
+    return ",".join(stationList)
+
+@app.route('/list_upbeat_stations/', methods=['GET'])
+def list_station():
+    stationsDict = app.config["UPBEAT"]
+    stationList = list(stationsDict.keys()) 
+    stationList.sort()
+    return ",".join(stationList)
+
+@app.route('/list_bible_stations/', methods=['GET'])
+def list_station():
+    stationsDict = app.config["BIBLE"]
     stationList = list(stationsDict.keys()) 
     stationList.sort()
     return ",".join(stationList)
